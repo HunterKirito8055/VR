1be28888bfb4054db62d8f699fe3fcf2d784b4f9
ashishpolaki <61271327+ashishpolaki@users.noreply.github.com>
2023-01-14 02:25:32 +0530

    Added Meditation Scene
    Added Meditation Scene

---
+++ Assets/Scripts/Meditation/BreatheController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class BreatheController : MonoBehaviour
+{
+    public Text breatheInValue;
+    public Text breatheOutValue;
+    public Button nextButton;
+    public GameObject panel;
+    public MeditationHale[] meditationHales;
+
+    private void Awake()
+    {
+        nextButton.onClick.AddListener(() => Next());
+    }
+
+    public void NextButtonActivation()
+    {
+        bool value = false;
+        if (String.IsNullOrEmpty(breatheInValue.text.ToString()) ||
+            String.IsNullOrEmpty(breatheOutValue.text.ToString()))
+        {
+            value = false;
+        }
+        else
+        {
+            if (float.IsNaN(float.Parse(breatheInValue.text.ToString())) ||
+                float.IsNaN(float.Parse(breatheOutValue.text.ToString())))
+            {
+                value = false;
+            }
+            else
+            {
+                value = true;
+            }
+        }
+        nextButton.gameObject.SetActive(value);
+    }
+
+    public void Next()
+    {
+        float breatheIn = float.Parse(breatheInValue.text.ToString());
+        float breatheOut = float.Parse(breatheOutValue.text.ToString());
+        foreach (var item in meditationHales)
+        {
+            item.OnBreatheValues(breatheIn, breatheOut);
+        }
+        panel.SetActive(true);
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Meditation/MeditationHale.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeditationHale : MonoBehaviour
+{
+    // public RectTransform timerText;
+    public RectTransform rectImage;
+    public float maxSize, minSize;
+    public float tempFloat;
+    public float inHale, exHale;
+    public bool isInHale;
+
+   
+    void Start()
+    {
+        isInHale = true;
+        rectImage.sizeDelta = new Vector2(minSize, minSize);
+    }
+    public void OnBreatheValues(float _inHale,float _exHale)
+    {
+        inHale = _inHale;
+        exHale = _exHale;
+    }
+    void FixedUpdate()
+    {
+        if (isInHale)
+        {
+            tempFloat += Time.fixedDeltaTime / inHale;
+            rectImage.sizeDelta = Vector2.Lerp(new Vector2(minSize, minSize), new Vector2(maxSize, maxSize), tempFloat);
+            if (tempFloat >= 1)
+            {
+                isInHale = false;
+                tempFloat = 0;
+            }
+        }
+        else
+        {
+            tempFloat += Time.fixedDeltaTime / exHale;
+            rectImage.sizeDelta = Vector2.Lerp(new Vector2(maxSize, maxSize), new Vector2(minSize, minSize), tempFloat);
+            if (tempFloat >= 1)
+            {
+                isInHale = true;
+                tempFloat = 0;
+            }
+        }
+    }
+}
