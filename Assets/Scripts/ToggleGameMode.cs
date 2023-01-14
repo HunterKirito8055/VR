bb642087393188d16be828b5893fbca0aa5050ed
ashishpolaki <61271327+ashishpolaki@users.noreply.github.com>
2023-01-14 22:12:01 +0530

    Fixed Gaze
    Fixed Gaze

--- Assets/Scripts/ToggleGameMode.cs
+++ Assets/Scripts/ToggleGameMode.cs
@@ -8,11 +8,11 @@ public class ToggleGameMode : MonoBehaviour
 {
     public void EnableVR(bool _enable)
     {
-        StartCoroutine(IEnableVRMode( _enable));
+        StartCoroutine(IEnableVRMode(_enable));
     }
     IEnumerator IEnableVRMode(bool _enable)
     {
-       //GameManager.Instance.IsVRMode = _enable;
+        //GameManager.Instance.IsVRMode = _enable;
         yield return null;
 
 #if !UNITY_EDITOR
@@ -31,6 +31,38 @@ public class ToggleGameMode : MonoBehaviour
 
         // Now it's ok to enable VR mode.
         XRSettings.enabled = _enable;
+
+
 #endif
     }
+    IEnumerator IDisableVRMode()
+    {
+        //GameManager.Instance.IsVRMode = _enable;
+        yield return null;
+
+        // https://docs.unity3d.com/ScriptReference/XR.XRSettings.LoadDeviceByName.html
+        // Device names are lowercase, as returned by `XRSettings.supportedDevices`.
+        string desiredDevice = "";
+
+        // Some VR Devices do not support reloading when already active, see
+        if (String.Compare(XRSettings.loadedDeviceName, desiredDevice, true) != 0)
+        {
+            XRSettings.LoadDeviceByName(desiredDevice);
+
+            // Must wait one frame after calling `XRSettings.LoadDeviceByName()`.
+            yield return null;
+        }
+
+        // Now it's ok to enable VR mode.
+        XRSettings.enabled = false;
+
+        load.ReloadCurrentScene();
+
+    }
+    public LoadSceneManager load;
+    public void DisableVR(bool _enable)
+    {
+        StartCoroutine(IDisableVRMode());
+    }
+
 }
